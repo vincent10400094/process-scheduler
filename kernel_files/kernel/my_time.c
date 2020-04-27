d0d4b8a32b13e8c4424b9f3be40c0f290a5df8bb
vincent <paintart.lin@gmail.com>
2020-04-27 13:30:35 +0800

    fix my_get_time syscall with vmware

--- kernel_files/kernel/my_time.c
+++ kernel_files/kernel/my_time.c
@@ -1,7 +1,10 @@
 #include <linux/linkage.h>
 #include <linux/kernel.h>
 #include <linux/timer.h>
+#include <linux/uaccess.h>
 
-asmlinkage void sys_my_time(struct timespec *t) {
-	getnstimeofday(t);
+asmlinkage void sys_my_time(struct timespec __user *t) {
+	struct timespec tt;
+	getnstimeofday(&tt);
+	copy_to_user(t, &tt, sizeof(struct timespec));
 }
